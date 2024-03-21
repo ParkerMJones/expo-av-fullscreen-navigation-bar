bc1f8bf75aa38298c85b71fea89a40e8dac85437
Parker Jones <parker.matthewjones@gmail.com>
2024-03-21 13:24:25 -0500

    basic example

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Pressable, Text } from "react-native";
+import React, { useRef, useState } from "react";
+import { Video, ResizeMode } from "expo-av";
+import * as NavigationBar from "expo-navigation-bar";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
+  const videoRef = useRef(null);
+  const [statusBarBackground, setStatusBarBackground] = useState("white");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor={statusBarBackground} />
+      <Video
+        ref={videoRef}
+        useNativeControls
+        source={{
+          uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+        }}
+        shouldPlay
+        resizeMode={ResizeMode.CONTAIN}
+        style={{
+          width: "100%",
+          height: 300,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onFullscreenUpdate={async (e) => {
+          console.log(e.fullscreenUpdate);
+          if (e.fullscreenUpdate === 0 || e.fullscreenUpdate === 1) {
+            await ScreenOrientation.unlockAsync();
+            await NavigationBar.setVisibilityAsync("hidden");
+            setStatusBarBackground("black");
+          }
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
